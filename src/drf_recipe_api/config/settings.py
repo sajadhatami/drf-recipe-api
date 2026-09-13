4f4dd1ce39fa797887570c129ba800c1ea39e36a
Sajad Hatami <sajadhatamiw@gmail.com>
2026-09-13 23:15:06 +0330

    feat: add django first app named "core"

--- src/drf_recipe_api/config/settings.py
+++ src/drf_recipe_api/config/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for config project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -73,9 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "PORT": "5432",
     }
 }
 
